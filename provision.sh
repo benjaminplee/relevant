7bdbe637d1cc9ab3d3b3843d4abb34f84d430b8a
leebenjp <leebenjp@gmail.com>
2020-01-23 02:43:43 +0000

    Quicken git setup

--- provision.sh
+++ provision.sh
@@ -8,3 +8,6 @@ sudo apt-get install git curl -y
 # Increase file handle limits
 sudo su -c 'echo -e "vagrant soft nofile 4096\nvagrant hard nofile 65536\n" > /etc/security/limits.d/vagrant.user.limits.conf'
 
+git config --global user.email "leebenjp@gmail.com"
+git config --global user.name "leebenjp"
+git config --global push.default simple
